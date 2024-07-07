8bc2bd7078dbfdf6f0adf9984cb3dd2089c7318f
kalio007 <kalioprince1@gmail.com>
2024-07-07 16:11:43 +0100

    added the general compose.yml

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,11 @@
-import { TanStackRouterVite } from "@tanstack/router-vite-plugin"
-import react from "@vitejs/plugin-react-swc"
-import { defineConfig } from "vite"
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), TanStackRouterVite()],
-})
+  server: {
+    host: "0.0.0.0",
+  },
+});
