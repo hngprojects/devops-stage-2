b0aae3f57426b60245ed83b2958f2cb065e9f8a5
root <root@ip-172-31-90-207.ec2.internal>
2024-07-07 13:32:55 +0000

    psuh all changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,8 @@ import { defineConfig } from "vite"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), TanStackRouterVite()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
 })
